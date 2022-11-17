83b0672d4ec07c2de67f5e490d0554fd84eee324
Ian Wang <wangin@uci.edu>
2022-11-16 17:13:49 -0800

    added HTML homepage

--- musicgeneratorproject/musicgeneratorapp/urls.py
+++ musicgeneratorproject/musicgeneratorapp/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.musicgenerator, name='musicgenerator'),
+    path('', views.homepage, name='homepage'),
 ]
--- musicgeneratorproject/musicgeneratorapp/views.py
+++ musicgeneratorproject/musicgeneratorapp/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 
 
 def musicgenerator(request):
     return HttpResponse("Hello world!")
+
+def homepage(request):
+    template = loader.get_template('homepage.html')
+    return HttpResponse(template.render())
