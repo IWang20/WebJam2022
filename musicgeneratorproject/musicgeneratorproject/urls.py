1510e6c19213cac28badd556f7105f010b8aac47
Noah Wang <botaow3@uci.edu>
2022-11-16 16:47:57 -0800

    Home Page

--- musicgeneratorproject/musicgeneratorproject/urls.py
+++ musicgeneratorproject/musicgeneratorproject/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('musicgeneratorapp/', include('musicgeneratorapp.urls'))
+    path('', include('musicgeneratorapp.urls'))
 ]
